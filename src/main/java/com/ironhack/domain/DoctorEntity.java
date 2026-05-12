dbd923c1247f70cdf5c7d7da2f2b5cd132faf16e
Omar Ismayilov <zephyr11ff@gmail.com>
2026-05-12 22:19:28 +0400

    feat(MED-16): Spotless style check configured. (#4)

--- src/main/java/com/ironhack/domain/DoctorEntity.java
+++ src/main/java/com/ironhack/domain/DoctorEntity.java
@@ -1,13 +1,13 @@
 package com.ironhack.domain;
 
+import java.util.UUID;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Entity
-@Table(name="doctors")
+@Table(name = "doctors")
 @Getter
 @Setter
 public class DoctorEntity {
@@ -15,10 +15,10 @@ public class DoctorEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name="full_name", nullable=false)
+    @Column(name = "full_name", nullable = false)
     private String fullName;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable=false)
+    @Column(nullable = false)
     private Specialty specialty;
 }
