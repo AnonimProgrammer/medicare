6d37c75b854976215f352b05cc56719949004587
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-05-13 16:47:51 +0400

    feat(MED-26): expose assign specialty endpoin

--- src/main/java/com/ironhack/infra/adapter/input/DoctorRestAdapter.java
+++ src/main/java/com/ironhack/infra/adapter/input/DoctorRestAdapter.java
@@ -1,27 +1,36 @@
 package com.ironhack.infra.adapter.input;
 
 import com.ironhack.application.dto.DoctorDTO;
+import com.ironhack.application.dto.request.AssignDoctorSpecialtyRequest;
 import com.ironhack.application.dto.request.CreateDoctorRequest;
 import com.ironhack.application.dto.response.ApiResponse;
+import com.ironhack.application.usecase.AssignSpecialtyToDoctorUseCase;
 import com.ironhack.application.usecase.CreateDoctorUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/doctors")
 @RequiredArgsConstructor
 public class DoctorRestAdapter {
     private final CreateDoctorUseCase createDoctorUseCase;
+    private final AssignSpecialtyToDoctorUseCase assignSpecialtyToDoctorUseCase;
 
     @PostMapping
     public ResponseEntity<ApiResponse<DoctorDTO>> createDoctor(@Valid @RequestBody CreateDoctorRequest request) {
         ApiResponse<DoctorDTO> doctor = createDoctorUseCase.invoke(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(doctor);
     }
+
+    @PatchMapping("/{id}/specialty")
+    public ResponseEntity<ApiResponse<DoctorDTO>> updateDoctor(@PathVariable UUID id, @Valid @RequestBody AssignDoctorSpecialtyRequest request) {
+        ApiResponse<DoctorDTO> body=assignSpecialtyToDoctorUseCase.invoke(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
 }
