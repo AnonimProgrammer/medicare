c85cedcc48ffaf658d9d3ce9ff144787b64bc8d9
AnonimProgrammer <zephyr11ff@gmail.com>
2026-05-15 03:12:52 +0400

    fix: Fix appointment time serialization issue with Jackson.

--- src/main/java/com/ironhack/application/dto/request/BookAppointmentRequest.java
+++ src/main/java/com/ironhack/application/dto/request/BookAppointmentRequest.java
@@ -3,9 +3,14 @@ package com.ironhack.application.dto.request;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import com.ironhack.infra.config.jackson.FlexibleOffsetDateTimeDeserializer;
 import jakarta.validation.constraints.NotNull;
+import tools.jackson.databind.annotation.JsonDeserialize;
 
 public record BookAppointmentRequest(
         @NotNull(message = "Patient ID is required") UUID patientId,
         @NotNull(message = "Doctor ID is required") UUID doctorId,
-        @NotNull(message = "Appointment time is required") OffsetDateTime appointmentTime) {}
+
+        @NotNull(message = "Appointment time is required")
+        @JsonDeserialize(using = FlexibleOffsetDateTimeDeserializer.class)
+        OffsetDateTime appointmentTime) {}
